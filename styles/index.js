028a8f90ced231d6031f1c417aff68cbfa6aa994
TOM BONIFACE <32570278+Bonifacetom1@users.noreply.github.com>
2021-12-14 01:44:27 +0100

    Add files via upload

---
+++ styles/index.js
@@ -0,0 +1,12 @@
+// alert("dfghj")
+// const openWebDev = document.getElementById('web-modal-btn');
+// const webModalContainer = document.getElementById('web_modal_container');
+// const closeWebModal = document.getElementById('close-web-modal');
+
+// openWebDev.addEventListener('click', ()=>{
+//     webModalContainer.classList.add('show')
+// });
+
+// closeWebModal.addEventListener('click', ()=>{
+//     webModalContainer.classList.remove('show')
+// });
